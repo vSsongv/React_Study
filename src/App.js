ae9c4bceecd44e655ce1ce223f04a0a1e59ccfa2
vSsongv <songdh21@naver.com>
2022-04-19 04:19:49 +0900

    🔥whyy??

--- src/App.js
+++ src/App.js
@@ -70,16 +70,48 @@ function Create(props) {
   );
 }
 
+function Update(props) {
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+
+  return (
+    <article>
+      <h2>Update</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          props.onUpdate(title, body);
+        }}>
+        <p>
+          <input type='text' name='title' value={title} onChange={(event) => setTitle(event.target.value)} />
+        </p>
+        <p>
+          <textarea name='body' value={body} onChange={(event) => setBody(event.target.value)}></textarea>
+        </p>
+        <p>
+          <input type='submit' value='Update' />
+        </p>
+      </form>
+    </article>
+  );
+}
+
 function App() {
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
+  const [nextId, setNextId] = useState(4);
 
-  const topics = [
+  const [topics, setTopics] = useState([
     { id: 1, title: 'html', body: 'html is ...' },
     { id: 2, title: 'css', body: 'css is ...' },
     { id: 3, title: 'js', body: 'js is ...' },
-  ];
+  ]);
+
   let content = null;
+  let contextControl = null;
+
   if (mode === 'WELCOME') {
     content = <Article title='Welcome' body='Hello, WEB'></Article>;
   } else if (mode === 'READ') {
@@ -92,8 +124,62 @@ function App() {
       }
     });
     content = <Article title={title} body={body}></Article>;
+    contextControl = (
+      <li>
+        <a
+          href={'/update/' + id}
+          onClick={(event) => {
+            event.preventDefault();
+            setMode('UPDATE');
+          }}>
+          Update
+        </a>
+      </li>
+    );
   } else if (mode === 'CREATE') {
-    content = <Create onCreate={(title, body) => {}}></Create>;
+    content = (
+      <Create
+        onCreate={(title, body) => {
+          const newTopics = [...topics, { id: nextId, title: title, body: body }];
+          setNextId(nextId + 1);
+          setTopics(newTopics);
+          setMode('Read');
+          setId(nextId);
+        }}></Create>
+    );
+  } else if (mode === 'UPDATE') {
+    let title,
+      body = null;
+    topics.forEach((topic) => {
+      if (topic.id === id) {
+        title = topic.title;
+        body = topic.body;
+      }
+    });
+    content = (
+      <Update
+        title={title}
+        body={body}
+        onUpdate={(title, body) => {
+          const newTopics = [...topics];
+          const updatedTopic = { id: id, title: title, body: body };
+          newTopics.forEach((topic) => {
+            if (topic.id === id) {
+              console.log(topic);
+              topic = updatedTopic;
+              console.log(topic);
+            }
+            console.log(newTopics);
+          });
+          // for (let i = 0; i < newTopics.length; i++) {
+          //   if (newTopics[i].id === id) {
+          //     newTopics[i] = updatedTopic;
+          //     break;
+          //   }
+          // }
+          setTopics(newTopics);
+        }}></Update>
+    );
   }
   return (
     <div className='App'>
@@ -109,14 +195,19 @@ function App() {
           setId(_id);
         }}></Nav>
       {content}
-      <a
-        href='/create'
-        onClick={(event) => {
-          event.preventDefault();
-          setMode('CREATE');
-        }}>
-        Create
-      </a>
+      <ul>
+        <li>
+          <a
+            href='/create'
+            onClick={(event) => {
+              event.preventDefault();
+              setMode('CREATE');
+            }}>
+            Create
+          </a>
+        </li>
+        {contextControl}
+      </ul>
     </div>
   );
 }
