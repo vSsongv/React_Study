7481cd1a924e9e4e0f669cb9979fe60238659082
vSsongv <songdh21@naver.com>
2022-04-25 22:44:01 +0900

    🛠️practice props

---
+++ src/Say.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const Say = () => {
+  const [message, setMessage] = useState('');
+  const [color, setColor] = useState('black');
+  const onClickEnter = () => setMessage('안녕하세요');
+  const onClickLeave = () => setMessage('잘가요');
+
+  return (
+    <div>
+      <button onClick={onClickEnter}>입장</button>
+      <button onClick={onClickLeave}>퇴장</button>
+      <h1 style={{ color }}>{message}</h1>
+
+      <button style={{ color: 'red' }} onClick={() => setColor('red')}>
+        Red
+      </button>
+      <button style={{ color: 'green' }} onClick={() => setColor('green')}>
+        Green
+      </button>
+      <button style={{ color: 'blue' }} onClick={() => setColor('blue')}>
+        Blue
+      </button>
+    </div>
+  );
+};
+
+export default Say;
