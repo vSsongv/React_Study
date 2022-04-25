b392e43d51052912f3ebc498bb8544544cce7b3b
vSsongv <songdh21@naver.com>
2022-04-25 23:35:00 +0900

    ⭐eventHandling practice

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,37 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class EventPractice extends Component {
-  state = {
+const EventPractice = () => {
+  const [form, setForm] = useState({
+    userName: '',
     message: '',
+  });
+
+  const { username, message } = form;
+  const onChange = (e) => {
+    const nextForm = {
+      ...form,
+      [e.target.name]: e.target.value,
+    };
+    console.log(nextForm);
+    console.log(form);
+    setForm(nextForm);
   };
 
-  render() {
-    return (
-      <div>
-        <h1>이벤트 연습</h1>
-        <input
-          type='text'
-          name='message'
-          placeholder='아무거나'
-          value={this.state.message}
-          onChange={(e) => {
-            this.setState({
-              message: e.target.value,
-            });
-          }}
-        />
-        <button
-          onClick={() => {
-            alert(this.state.message);
-            this.setState({
-              message: '',
-            });
-          }}>
-          확인
-        </button>
-      </div>
-    );
-  }
-}
+  const onClick = () => {
+    alert(username + ':' + message);
+    setForm({
+      userName: '',
+      message: '',
+    });
+  };
+
+  const onKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      onClick();
+    }
+  };
+
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input type='text' name='message' placeholder='아무거나' value={message} onChange={onChange} onKeyPress={onKeyPress} />
+      <button onClick={onClick}>확인</button>
+    </div>
+  );
+};
 
 export default EventPractice;
