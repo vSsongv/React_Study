7e2d41b867f9e673b4d36ac3efae3a0535a902df
vSsongv <songdh21@naver.com>
2022-04-27 23:31:59 +0900

    🛠️practice dom - js

---
+++ src/validationSample.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './validationSample.css';
+
+class validationSample extends Component {
+  state = {
+    pwd: '',
+    clicked: false,
+    validate: false,
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      pwd: e.target.value,
+    });
+  };
+  handleButton = (e) => {
+    this.setState({
+      clicked: true,
+      validated: this.state.pwd === '0000',
+    });
+  };
+  render() {
+    return <div></div>;
+  }
+}
+
+export default validationSample;
