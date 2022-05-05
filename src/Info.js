b88bef976504e3cab2f4fe4ef1e2f75757fd7f74
vSsongv <songdh21@naver.com>
2022-05-06 06:27:15 +0900

    ✨useReducer

--- src/Info.js
+++ src/Info.js
@@ -1,32 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
 
 const Info = () => {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState();
-  useEffect(() => {
-    console.log('렌더링완료');
-    console.log(name, nickname);
-  });
-
-  const onChangeName = (e) => {
-    setName(e.target.value);
+  const [state, dispatch] = useReducer(reducer, { name: '', nickname: '' });
+  const { name, nickname } = state;
+  const onChange = (e) => {
+    dispatch(e.target);
   };
+  // const [name, setName] = useState('');
+  // const [nickname, setNickname] = useState();
+  // useEffect(() => {
+  //   console.log('렌더링완료');
+  //   console.log(name, nickname);
+  // });
 
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
-  };
+  // const onChangeName = (e) => {
+  //   setName(e.target.value);
+  // };
+
+  // const onChangeNickname = (e) => {
+  //   setNickname(e.target.value);
+  // };
 
   return (
     <div>
-      <input value={name} onChange={onChangeName} />
-      <input value={nickname} onChange={onChangeNickname} />
+      <input value={name} onChange={onChange} />
+      <input value={nickname} onChange={onChange} />
       <div>
         <div>
           <b>이름:</b>
           {name}
         </div>
       </div>
-
       <div>
         <div>
           <b>닉네임:</b>
