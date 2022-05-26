ad52c0828beb095cb47e4f0bc34607b0d594ba71
vSsongv <songdh21@naver.com>
2022-05-26 10:36:33 +0900

    ✨styling done

---
+++ styling-react/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import StyledComponent from '../src/StyledComponent';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <StyledComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
