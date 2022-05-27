2145b19c3de9be8eaa6475ceb5a42c1e4e2be2a6
vSsongv <songdh21@naver.com>
2022-05-28 06:20:39 +0900

    ✨first todo Done!

---
+++ todo-app/src/App.js
@@ -0,0 +1,56 @@
+import React, { useCallback, useRef, useState } from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+
+const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: '리액트 기초 알기', checked: true },
+    { id: 2, text: '스타일링 기초 알기', checked: true },
+    { id: 3, text: '훅 기초 알기', checked: false },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+
+  return (
+    <div>
+      <TodoTemplate>
+        <TodoInsert onInsert={onInsert} />
+        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+      </TodoTemplate>
+    </div>
+  );
+};
+
+export default App;
